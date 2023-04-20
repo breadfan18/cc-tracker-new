f74df05840a8fadf2c3cfede32eff1466e4c669c
Swaroop Uprety <breadfan18@gmail.com>
2023-04-20 17:39:16 -0400

    add course and author count to header

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,7 +1,10 @@
 import React from "react";
+import { connect } from "react-redux";
 import { NavLink } from "react-router-dom";
+import { HeaderSpinner, Spinner } from "./Spinner";
+import PropTypes from "prop-types";
 
-const Header = () => {
+const Header = ({ authors, courses, loading }) => {
   const activeStyle = { color: "#F15B2A" };
   return (
     <nav>
@@ -9,19 +12,33 @@ const Header = () => {
         Home
       </NavLink>
       {" | "}
-      <NavLink to="/courses" activeStyle={activeStyle}>
-        Courses
-      </NavLink>
-      {" | "}
       <NavLink to="/about" activeStyle={activeStyle}>
         About
       </NavLink>
       {" | "}
+      <NavLink to="/courses" activeStyle={activeStyle}>
+        Courses {loading ? <HeaderSpinner /> : courses.length}
+      </NavLink>
+      {" | "}
       <NavLink to="/authors" activeStyle={activeStyle}>
-        Authors
+        Authors {loading ? <HeaderSpinner /> : authors.length}
       </NavLink>
     </nav>
   );
 };
 
-export default Header;
+Header.propTypes = {
+  authors: PropTypes.array.isRequired,
+  courses: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  authors: state.authors,
+  courses: state.courses,
+  loading: state.apiCallsInProgress > 0,
+});
+
+const mapDispatchToProps = {};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
