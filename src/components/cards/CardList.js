1c6ac3de5e7022e27c9f2f763379e6f961db7957
Swaroop Uprety <breadfan18@gmail.com>
2023-06-07 19:05:40 -0400

    sorting almost done

--- src/components/cards/CardList.js
+++ src/components/cards/CardList.js
@@ -1,32 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import EmptyList from "../common/EmptyList";
 import Table from "react-bootstrap/Table";
+import { TiArrowUnsorted } from "react-icons/ti";
 
 const CardList = ({ cards, onDeleteClick, deletedCard, showEditDelete }) => {
+  let sortedCards = [...cards];
+  const [sortConfig, setSortConfig] = useState({
+    key: null,
+    direction: null,
+  });
+
+  if (sortConfig.key !== null) {
+    sortedCards.sort((a, b) => {
+      if (a[sortConfig.key] < b[sortConfig.key]) {
+        return sortConfig.direction === "ascending" ? -1 : 1;
+      }
+      if (a[sortConfig.key] > b[sortConfig.key]) {
+        return sortConfig.direction === "ascending" ? 1 : -1;
+      }
+      return 0;
+    });
+  }
+
+  const requestSort = (key) => {
+    let direction = "ascending";
+    if (sortConfig.key === key && sortConfig.direction === "ascending") {
+      direction = "descending";
+    }
+    setSortConfig({ key, direction });
+  };
+
   return cards.length === 0 ? (
     <EmptyList dataType={"card"} />
   ) : (
     <Table size="sm">
       <thead>
         <tr>
-          <th>App Date</th>
-          <th>User</th>
-          <th>Issuer</th>
-          <th>Card</th>
-          <th>Type</th>
+          <th className="tableHeader">
+            App Date
+            <TiArrowUnsorted onClick={() => requestSort("appDate")} />
+          </th>
+          <th className="tableHeader">
+            User
+            <TiArrowUnsorted onClick={() => requestSort("userName")} />
+          </th>
+          <th className="tableHeader">
+            Issuer <TiArrowUnsorted onClick={() => requestSort("issuer")} />
+          </th>
+          <th className="tableHeader">
+            Card <TiArrowUnsorted onClick={() => requestSort("card")} />
+          </th>
+          <th className="tableHeader">
+            Type <TiArrowUnsorted onClick={() => requestSort("cardType")} />
+          </th>
           {showEditDelete && (
             <>
-              <th>Delete?</th>
-              <th>Modify?</th>
+              <th></th>
+              <th></th>
             </>
           )}
         </tr>
       </thead>
       <tbody>
-        {cards.map((card) => {
+        {sortedCards.map((card) => {
           const isCardDeleted = card.id === deletedCard?.id;
           return (
             <tr key={card.id}>
--- src/helpers.js
+++ src/helpers.js
@@ -20,3 +20,19 @@ export function addUserNameToCard(card) {
 export function sortCardsByDate(cards) {
   return cards.sort((a, b) => Date.parse(a.appDate) - Date.parse(b.appDate));
 }
+
+export function sortCardsByName(cards) {
+  return cards.sort((a, b) => {
+    const nameA = a.userName.toUpperCase(); // ignore upper and lowercase
+    const nameB = b.userName.toUpperCase(); // ignore upper and lowercase
+    if (nameA < nameB) {
+      return -1;
+    }
+    if (nameA > nameB) {
+      return 1;
+    }
+
+    // names must be equal
+    return 0;
+  });
+}
