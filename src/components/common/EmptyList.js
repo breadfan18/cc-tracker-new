f0f593add8fc8ee4072f8bc321846bcee49c9765
Swaroop Uprety <breadfan18@gmail.com>
2023-06-25 03:14:09 -0400

    emptylist setup for loyalty

--- src/components/common/EmptyList.js
+++ src/components/common/EmptyList.js
@@ -3,22 +3,25 @@ import { AiOutlineUserAdd } from "react-icons/ai";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 
-const EmptyList = ({ dataType }) => (
-  <div className="alert alert-danger" role="alert">
-    <p style={{ marginBottom: "0" }}>
-      No {`${dataType}s`} to display. Click{" "}
-      <Link to={`/${dataType}/`} style={{ textDecoration: "none" }}>
-        here
-      </Link>{" "}
-      to add a new {dataType}.
-    </p>
-    <AiOutlineUserAdd
-      className="closeIcon"
-      onClick={() => console.log("modalHeader")}
-      style={{ cursor: "pointer" }}
-    />
-  </div>
-);
+const EmptyList = ({ dataType }) => {
+  const route = dataType === "card" ? "card" : "loyalty";
+  return (
+    <div className="alert alert-danger" role="alert">
+      <p style={{ marginBottom: "0" }}>
+        No {`${dataType}s`} to display. Click{" "}
+        <Link to={`/${route}/`} style={{ textDecoration: "none" }}>
+          here
+        </Link>{" "}
+        to add a new {dataType}.
+      </p>
+      <AiOutlineUserAdd
+        className="closeIcon"
+        onClick={() => console.log("modalHeader")}
+        style={{ cursor: "pointer" }}
+      />
+    </div>
+  );
+};
 
 EmptyList.propTypes = {
   dataType: PropTypes.string.isRequired,
--- src/components/loyalty/LoyaltyTabs.js
+++ src/components/loyalty/LoyaltyTabs.js
@@ -4,16 +4,17 @@ import Tabs from "react-bootstrap/Tabs";
 import PropTypes from "prop-types";
 import LoyaltyList from "./LoyaltyList";
 import { connect } from "react-redux";
-import { USERS } from "../../constants";
+import { ACCOUNT_TYPE, USERS } from "../../constants";
 import { titleCase } from "../../helpers";
 import CustomAccordion from "../common/CustomAccordion";
 import { Card } from "react-bootstrap";
 import _ from "lodash";
 import LoyaltyCards from "./LoyaltyCards";
+import EmptyList from "../common/EmptyList";
 
 function LoyaltyTabs({ loyaltyData, windowWidth }) {
   const loyaltyByType = _.groupBy(loyaltyData, (o) => o.loyaltyType);
-  const loyaltyTabs = Object.keys(loyaltyByType).map((loyaltyType) => {
+  const loyaltyTabs = ACCOUNT_TYPE.map((loyaltyType) => {
     const loyaltyTypeData = loyaltyByType[loyaltyType];
     const loyaltyTypePerUser = _.groupBy(loyaltyTypeData, (o) => o.userId);
     const userCards = Object.keys(loyaltyTypePerUser).map((user) => {
@@ -65,7 +66,11 @@ function LoyaltyTabs({ loyaltyData, windowWidth }) {
         title={titleCase(loyaltyType)}
         key={loyaltyType}
       >
-        {userCards}
+        {userCards.length === 0 ? (
+          <EmptyList dataType="loyalty account" />
+        ) : (
+          userCards
+        )}
       </Tab>
     );
   });
