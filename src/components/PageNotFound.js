8f7c8bb9ce8ae46b1f47d9818b21dcc285b3da6f
Swaroop Uprety <breadfan18@gmail.com>
2023-06-25 02:46:25 -0400

    misc cleanup

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-const PageNotFound = () => <h1>Oops! That page does not exist</h1>
+const PageNotFound = () => (
+  <h1 className="sectionHeaders">Oops! That page does not exist</h1>
+);
 
-export default PageNotFound
-  
-
+export default PageNotFound;
